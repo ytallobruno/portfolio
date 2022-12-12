daa3e5cbbc724409e6b196f19f771450923098b7
Ytallo <ytallobruno@hotmail.com>
2022-12-12 19:08:04 -0300

    FIX: Ajustes gerais

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-      <NavBar/>
-        <div className="App">
-          <p>hello world</p>
-        </div>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="App">
+        <p>hello world</p>
+      </div>
+    </>
+  );
 }
