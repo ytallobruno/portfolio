66d2a8e7310226af7a93e3eafaa646cb5e9d8d6e
Ytallo <ytallobruno@hotmail.com>
2022-12-22 16:53:04 -0300

    checkpoint

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Experience() {
   return (
+    // <div id="experience" className="bg-gradient-to-b from-slate-900 to-slate-800 h-screen">
     <div id="experience" className="bg-slate-400 h-screen">
       oooooooi
     </div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Portfolio() {
   return (
-    <div id="portfolio" className="bg-slate-500 h-screen">Projects</div>
+    <div id="portfolio" className="bg-gradient-to-b from-slate-800 to-slate-700 h-screen">Projects</div>
   )
 }
